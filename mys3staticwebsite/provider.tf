19530c55ade243582c1418bfca9c7b10878885a3
Kurt <ojt.kfegarido@sminvestments.com>
2026-02-03 17:02:35 +0800

    add initial Terraform configuration for S3 bucket setup and Jenkins integration

---
+++ mys3staticwebsite/provider.tf
@@ -0,0 +1,13 @@
+# Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
